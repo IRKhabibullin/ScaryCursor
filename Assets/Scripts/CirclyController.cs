6859b9b69ed0fe64fa4f0db726b3f2973a6a6ce7
IRKhabibullin <36387914+IRKhabibullin@users.noreply.github.com>
2021-11-04 14:39:51 +0300

    running away from cursor

--- Assets/Scripts/CirclyController.cs
+++ Assets/Scripts/CirclyController.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class CirclyController : MonoBehaviour
 {
     [SerializeField] private LayerMask finishAreaLayer;
+    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform finishArea;
+    [SerializeField] private float fleeRadius;
     private NavMeshAgent agent;
+    public TextMeshProUGUI debugText;
 
     void Start()
     {
@@ -16,7 +20,25 @@ public class CirclyController : MonoBehaviour
 
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+        {
+            debugText.text = hit.point.ToString();
+            if (Vector3.Distance(transform.position, hit.point) < fleeRadius)
+            {
+                Vector3 newDestination = transform.position * 2 - hit.point;
+                agent.SetDestination(newDestination);
+            }
+        }
+        /*Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+        mousePosition.y = 0;
+        debugText.text = mousePosition.ToString();
+        if (Vector3.Distance(transform.position, mousePosition) < fleeRadius)
+        {
+            Vector3 newDestination = transform.position * 2 - mousePosition;
+            agent.SetDestination(newDestination);
+        }*/
     }
 
     private void OnTriggerEnter(Collider other)
